16d3dde71598a084575b7b1523dfebc5714b149b
James Kyburz <james.kyburz@gmail.com>
2014-10-20 17:46:49 +0200

    Revert "Don't rotate added as a default option"
    This reverts commit 8ca2036765bd95294804f741a4e5931ca7531669.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var duplexer = require('duplexer');
 module.exports = ghostify;
 
 function ghostify() {
-  var pdf2ps = spawn('pdf2ps', ['-sAutoRotatePage=PageByPage','-sAutoRotatePage=None', '-', '-']);
+  var pdf2ps = spawn('pdf2ps', ['-', '-']);
   var ps2pdf = spawn('ps2pdf', ['-', '-']);
   pdf2ps.stdout.pipe(ps2pdf.stdin);
   return duplexer(pdf2ps.stdin, ps2pdf.stdout);
