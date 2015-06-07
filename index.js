b119d2c1dedf6f913025885962676e1330bb4470
James Kyburz <james.kyburz@gmail.com>
2015-06-07 20:28:26 +0200

    pipe stderr to see when fails

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function ghostify() {
   var pdf2ps = spawn('pdf2ps', ['-', '-']);
   var ps2pdf = spawn('ps2pdf', ['-', '-']);
   pdf2ps.stdout.pipe(ps2pdf.stdin);
+  pdf2ps.stderr.pipe(process.stderr);
   return duplexer(pdf2ps.stdin, ps2pdf.stdout);
 }
