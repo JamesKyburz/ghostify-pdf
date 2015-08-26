0e9353ede82761e609b5ba83cf3811f2050227f6
James Kyburz <james.kyburz@gmail.com>
2015-08-26 22:55:44 +0200

    Don't automatically rotate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = ghostify;
 
 function ghostify() {
   var pdf2ps = spawn('pdf2ps', ['-', '-']);
-  var ps2pdf = spawn('ps2pdf', ['-', '-']);
+  var ps2pdf = spawn('ps2pdf', ['-dAutoRotatePages=/None', '-', '-']);
   pdf2ps.stdout.pipe(ps2pdf.stdin);
   pdf2ps.stderr.pipe(process.stderr);
   return duplexer(pdf2ps.stdin, ps2pdf.stdout);
