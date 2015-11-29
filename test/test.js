2007d1f114b235d43ac89d25297102a108c6f9c2
James Kyburz <james.kyburz@gmail.com>
2015-11-29 21:31:22 +0100

    1.0.5

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-var ghostify = require('../');
-var fs = require('fs');
-var test = require('tape');
+var ghostify = require('../')
+var fs = require('fs')
+var test = require('tape')
 
-test('verify it runs and doesn\'t blow up', function(t) {
-  var file = fs.createReadStream(__dirname + '/test.pdf');
-  var fix = ghostify();
-  fix.pipe(process.stdout);
-  file.pipe(fix);
-  fix.on('end', function() {
-    t.ok(true, 'If we got here hopefully all is good');
-    t.end();
-  });
-});
+test("verify it runs and doesn't blow up", function (t) {
+  var file = fs.createReadStream(__dirname + '/test.pdf')
+  var fix = ghostify()
+  fix.pipe(process.stdout)
+  file.pipe(fix)
+  fix.on('end', function () {
+    t.ok(true, 'If we got here hopefully all is good')
+    t.end()
+  })
+})
