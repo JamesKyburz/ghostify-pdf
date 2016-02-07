0c4065f1e4faa27108925b5ee5a1a6c97b388819
James Kyburz <james.kyburz@gmail.com>
2016-02-07 14:57:06 +0100

    standard path fix

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 var ghostify = require('../')
 var fs = require('fs')
 var test = require('tape')
+var path = require('path')
 
 test("verify it runs and doesn't blow up", function (t) {
-  var file = fs.createReadStream(__dirname + '/test.pdf')
+  var file = fs.createReadStream(path.join(__dirname, '/test.pdf'))
   var fix = ghostify()
   fix.pipe(process.stdout)
   file.pipe(fix)
